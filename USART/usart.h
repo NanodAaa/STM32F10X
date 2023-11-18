66d19aef933af73afa7b4ccde5e7deb8a1443706
NanodAaa <102843392+NanodAaa@users.noreply.github.com>
2023-11-18 14:11:30 +0100

    Update usart.h

--- USART/usart.h
+++ USART/usart.h
@@ -1,16 +1,13 @@
 #ifndef _USART_H_
 #define _USART_H_
 
-
 #include "stm32f10x.h"
 #include "board.h"
 #include "NANODA.h"
 
-
 // 串口波特率
 #define USART_BAUDRATE_9600		9600
 #define USART_BAUDRATE_115200	115200
-
 
 // 串口发送/接收缓冲区大小
 #define USART_TX_BUF_SIZE 512
@@ -24,28 +21,7 @@ typedef enum _tagUSARTNum
 	USART_NUM_3,
 }TUSARTNum;
 
-
 void UsartInitial(TUSARTNum USARTnum, uint32_t uiBaudRate);
 void DEBUGOutput(TUSARTNum USARTNum, BYTE* p_uiData);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 #endif
