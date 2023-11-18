e5d6c5f0e2b54e8b75e76c9fea424b2b363ce948
NanodAaa <102843392+NanodAaa@users.noreply.github.com>
2023-11-18 14:10:20 +0100

    Update TIM.h

--- TIM/TIM.h
+++ TIM/TIM.h
@@ -5,7 +5,6 @@
 #include "NANODA.h"
 #include "board.h"
 
-
 // TIM定时初值
 #define TIM_10MS_PERIOD 	72
 #define TIM_10MS_PRESCALER 	10000
@@ -13,11 +12,6 @@
 #define TIM_500MS_PERIOD	5000
 #define TIM_500MS_PRESCALER	7200
 
-
-
-
-
 void TIM3Initial(uint16_t uiPeriod, uint16_t uiPrescaler);
-
 
 #endif
